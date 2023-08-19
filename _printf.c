a51d9ee5059ce687418e1c1aa46d150b33587a69
Andrew Larpei <andrewlarpei@gmail.com>
2023-08-19 17:36:02 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,6 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
+ Change updated
  */
 int _printf(const char *format, ...)
 {
